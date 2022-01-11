260c7b4c86a2862530bde7390215f8a2f9c87a65
dhruvillsangani <dhruvil.s@prominentpixel.com>
2022-01-11 10:30:56 +0530

    10-01

--- 5. Working with Express.js/Express Intro/app.js
+++ 5. Working with Express.js/Express Intro/app.js
@@ -4,8 +4,25 @@ const routes = require('./Routes/routes');
 const app = express();
 
 
-app.use(routes);
 
+
+app.use((req, res, next) => {
+    let validIps = ['127.0.0.1','::1','192.168.43.87']; // Put your IP whitelist in this array
+    
+      if(validIps.includes(req.ip)){
+        
+          console.log("Authorize");
+          next();
+      }
+      else
+      {
+          
+          console.log("unauthorize: " + req,ip);
+          const err = new Error("unauthorize: " + req.ip);
+          next(err);
+      }
+    })
+    app.use(routes);
 app.use('/',(req,res,next)=>{
     res.status(404).send('<h1>page not found</h1>');
 });
