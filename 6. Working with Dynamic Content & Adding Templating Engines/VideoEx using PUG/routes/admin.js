380efd9e19d6b6a888822c58d614da5e4e71f6e3
dhruvillsangani <dhruvil.s@prominentpixel.com>
2022-01-11 19:20:14 +0530

    11/01/2022

---
+++ 6. Working with Dynamic Content & Adding Templating Engines/VideoEx using PUG/routes/admin.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const rootDir = require('../util/path');
+const router = express.Router();
+const products = [];
+
+
+
+router.get('/add-product', (req, res, next) => {
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product')
+});
+
+
+router.post('/add-product', (req, res, next) => {
+  products.push({ name: req.body.name,id:req.body.id,department:req.body.department });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
