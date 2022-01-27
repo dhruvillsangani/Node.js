f005e0a92b8bedd766e56681d8f11dface1ccc54
dhruvillsangani <dhruvil.s@prominentpixel.com>
2022-01-27 19:50:58 +0530

    27/01/2022

--- Ch 12/Exersise12/controllers/student.js
+++ Ch 12/Exersise12/controllers/student.js
@@ -1,12 +1,15 @@
 
 const Student = require('../models/student_details')
 const certificate = require('../models/certificate')
+const Marks = require('../models/marks');
 
 exports.getStudents = (req,res,next) => {
     // For add student forms 
     certificate.fetchAll()
+
     .then((result) => {
         console.log(result);
+       
         res.render('addStudent',{prods:result})
       })
       .catch(err => console.log(err));
@@ -14,14 +17,21 @@ exports.getStudents = (req,res,next) => {
 }
 
 exports.postStudent= (req,res,next) => {
-    const students = new Student(req.body.name,req.body.surname,[req.body.certificateid]);
-    
+    const students = new Student(req.body.id,req.body.name,req.body.surname,req.body.certificateid);
+    var english = parseInt(req.body.english);
+    var maths = parseInt(req.body.maths);
+    var science = parseInt(req.body.science)
+    var persentage =(english+maths+science)/3;
+    const marks = new Marks(persentage,req.body.english,req.body.maths,req.body.science,req.body.id)
+    marks.save()
     students.save()
     .then((result) => {
         console.log(result);
         res.redirect('/');
       })
       .catch(err => console.log(err));
+
+    
 
 }
 
@@ -85,3 +95,35 @@ exports.deleteStudent = (req,res,next) => {
 
 
 
+
+
+// for learning purpose 
+exports.postmarks =  (req,res,next) => { 
+    let id = req.params.id;
+    
+
+    Marks.findById(id)
+    .then((result) => {
+         var result1 = result;
+        //  res.json(result);
+        res.render('studentByMarks',{student:result})
+     console.log(result1[0].studentId);
+    })
+    
+    .catch(err => console.log("err : "+err));
+
+}
+
+
+exports.postcertificate =  (req,res,next) => {
+    let id = req.params.id;
+    Student.fetchByCertificateId(id)
+
+    .then((result) => {
+        console.log(result);
+       
+        res.json(result)
+      })
+      .catch(err => console.log(err));
+
+} 
